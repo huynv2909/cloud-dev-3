a5af9a9319e622b1b7748796e0347781682f8e1f
huynv2909 <huynv2909@gmail.com>
2023-06-14 02:12:10 +0700

    add content

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac851bac8005340ab8a473ed6381cd29-1413667513.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
